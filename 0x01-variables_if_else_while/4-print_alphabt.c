546a4333de038e4ca602c3059dcd3a70a955fcb2
eshon2k <shongaec@gmail.com>
2023-02-19 15:23:40 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,10 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
 		putchar(c);
-	for (c = 'A'; c <= 'Z'; c++)
-		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
