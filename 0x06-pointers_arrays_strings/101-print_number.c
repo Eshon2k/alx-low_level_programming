083ede101d99d9f2792956104ee44a263e837d38
eshon2k <shongaec@gmail.com>
2023-03-02 18:01:18 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,57 @@
 #include "main.h"
+
 /**
- *  print_number - Prints an integer.
- * @n: The integer to be printed.
- *
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer to input string.
+ * Return: Returns pointer to capitalized string.
  */
 
-void print_number(int n)
-
+char *cap_string(char *s)
 {
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	unsigned int num = n;
-
-	if (n < 0)
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-	_putchar('-');
-	num = -num;
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
+		i++;
+	}
+	return (s);
+}
+/*
+ * {
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
 
-	if ((num / 10) > 0)
-	print_number(num / 10);
-	_putchar((num % 10) + '0');
-
+	return (str);
 }
+*/
+
